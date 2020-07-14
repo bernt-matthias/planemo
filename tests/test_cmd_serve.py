b243cbfae0a62adefe3f461543901255ac40ec8b
Matthias Bernt <m.bernt@ufz.de>
2020-07-14 12:08:44 +0200

    1

--- tests/test_cmd_serve.py
+++ tests/test_cmd_serve.py
@@ -40,6 +40,8 @@ class ServeTestCase(CliTestCase):
         super(ServeTestCase, self).setUp()
         self._port = network_util.get_free_port()
         self._pid_file = os.path.join(self._home, "test.pid")
+        fh = open(self._pid_file, "w")
+        fh.close()
         self._serve_artifact = os.path.join(TEST_REPOS_DIR, "single_tool", "cat.xml")
 
     @skip_if_environ("PLANEMO_SKIP_GALAXY_TESTS")
